c991214e79e368c311965d79a0447ae3d84cc468
from97 <skm1104@daum.net>
2021-07-06 19:30:55 +0900

    ProfileView 컴포넌트 작성 및 스타일링

--- src/components/ProfileView/ProfileView.styles.jsx
+++ src/components/ProfileView/ProfileView.styles.jsx
@@ -1,5 +1,94 @@
 import styled from 'styled-components';
 
-const ProfileViewStyled = styled.div``;
+export const ProfileViewStyled = styled.div`
+	width: 300px;
+	height: 490px;
+	background: #fff;
+	border: 1px solid #dadcf3;
+	box-sizing: border-box;
+	border-radius: 35px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 0 53px 0 95px;
+`;
 
-export default ProfileViewStyled;
+export const ProfileImage = styled.div`
+	width: 140px;
+	height: 140px;
+	border: 1px solid #dadcf3;
+	box-sizing: border-box;
+	border-radius: 100%;
+	margin: 32px 0 23px 0;
+`;
+
+export const UserInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+UserInfo.Name = styled.div`
+	font-style: normal;
+	font-weight: bold;
+	font-size: 20px;
+	line-height: 24px;
+	color: #000000;
+	margin-bottom: 4px;
+`;
+
+UserInfo.Job = styled.div`
+	font-style: normal;
+	font-weight: 300;
+	font-size: 12px;
+	line-height: 14px;
+	color: #9f9f9f;
+`;
+
+UserInfo.Description = styled.div`
+	font-style: normal;
+	font-weight: 300;
+	font-size: 12px;
+	line-height: 18px;
+	color: #000000;
+	width: 214px;
+	margin: 21px 0 30px 0;
+`;
+
+export const ModifyProfileButton = styled.button`
+	background: #ffffff;
+	width: 214px;
+	height: 48px;
+	border: 0.5px solid #b9b9b9;
+	box-sizing: border-box;
+	border-radius: 50px;
+	font-style: normal;
+	font-weight: 200;
+	font-size: 14px;
+	line-height: 18px;
+	color: #000000;
+
+	:hover {
+		background: rgba(218, 220, 243, 0.25);
+		border: 0.5px solid #dadcf3;
+		box-sizing: border-box;
+		border-radius: 50px;
+	}
+`;
+
+export const SNSList = styled.ul`
+	display: flex;
+	align-items: center;
+	height: 87px;
+	margin-top: 14px;
+`;
+
+SNSList.Item = styled.li`
+	width: 37px;
+	height: 37px;
+	background: #f5f6ff;
+	border: 1px solid #c9cdfa;
+	box-sizing: border-box;
+	border-radius: 6px;
+	margin: 0 11px 0 11px;
+`;
