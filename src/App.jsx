2472b7be7b617c4ef6c0f916626c24f25f5c2b1e
from97 <skm1104@daum.net>
2021-07-06 19:27:39 +0900

    GlobalStyle html, body, #root 지정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import { createGlobalStyle } from 'styled-components';
 import MainPage from './pages/MainPage';
 
 const GlobalStyle = createGlobalStyle`
-  ${resetCss};
+	${resetCss};  
+	html, body {
+		height: 100%;
+		background-color: #F5F7FE;
+	}
+
+	#root {
+		width: 100%;
+		height: 100%;
+		font-family: Apple SD Gothic Neo;
+		font-weight: normal;
+	}
 `;
 
 function App() {
