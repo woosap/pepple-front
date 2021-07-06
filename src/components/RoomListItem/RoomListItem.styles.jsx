18c3958ab980a8e1a77366bb75f515016568c43d
from97 <skm1104@daum.net>
2021-07-06 20:17:10 +0900

    RoomListItem 컴포넌트 컴파일 에러 해결

--- src/components/RoomListItem/RoomListItem.styles.jsx
+++ src/components/RoomListItem/RoomListItem.styles.jsx
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const RoomListItemStyled = styled.div``;
 
 export const RoomListItemBox = styled.div``;
+
+RoomListItemBox.Title = styled.div``;
+
+RoomListItemBox.Subtitle = styled.div``;
+
+RoomListItemBox.BirthTime = styled.div``;
