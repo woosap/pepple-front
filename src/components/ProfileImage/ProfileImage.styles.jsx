00e22ebeb73ca87c90fe5926316c5fa3f7e5bd07
from97 <skm1104@daum.net>
2021-07-06 20:17:22 +0900

    ProfileImage 컴포넌트 생성

---
+++ src/components/ProfileImage/ProfileImage.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ProfileImageStyled = styled.div`
+	width: 140px;
+	height: 140px;
+	border: 1px solid #dadcf3;
+	box-sizing: border-box;
+	border-radius: 100%;
+	margin: 32px 0 23px 0;
+`;
+
+export default ProfileImageStyled;
