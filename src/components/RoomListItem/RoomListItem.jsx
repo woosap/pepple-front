c3576cb8138a8718eefcce7ca93370c618060208
from97 <skm1104@daum.net>
2021-07-06 20:11:20 +0900

    RoomListItem 컴포넌트 작성

--- src/components/RoomListItem/RoomListItem.jsx
+++ src/components/RoomListItem/RoomListItem.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import RoomListItemStyled from './RoomListItem.styles';
+import { RoomListItemStyled, RoomListItemBox } from './RoomListItem.styles';
 
 const RoomListItem = () => {
-	return <RoomListItemStyled>Room</RoomListItemStyled>;
+	return (
+		<RoomListItemStyled>
+			<RoomListItemBox>
+				<RoomListItemBox.Title />
+				<RoomListItemBox.Subtitle />
+				<RoomListItemBox.BirthTime />
+			</RoomListItemBox>
+		</RoomListItemStyled>
+	);
 };
 
 export default RoomListItem;
--- src/components/RoomListItem/RoomListItem.styles.jsx
+++ src/components/RoomListItem/RoomListItem.styles.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
-const RoomListItemStyled = styled.div``;
+export const RoomListItemStyled = styled.div``;
 
-export default RoomListItemStyled;
+export const RoomListItemBox = styled.div``;
