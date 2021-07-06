bf72b6b016b9bdc790e1d4fce405249d0108d00a
from97 <skm1104@daum.net>
2021-07-06 18:45:42 +0900

    ProfileView 생성 및 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import ProfileView from '../components/ProfileView/ProfileView';
 
-const MainPage = () => <Header />;
+const MainPage = () => {
+	return (
+		<>
+			<Header />
+			<ProfileView />
+		</>
+	);
+};
 
 export default MainPage;
