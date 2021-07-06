2a6192a573ab3075335fbada9edd2cbf565f3e21
from97 <skm1104@daum.net>
2021-07-06 21:01:46 +0900

    RoomCategory 컴포넌트 생성

---
+++ src/components/RoomCategory/RoomCategory.styles.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const RoomCategoryStyled = styled.div`
+	width: 50px;
+	height: 19px;
+	left: 473px;
+	top: 201px;
+	background: #6138b9;
+	border-radius: 50px;
+	margin-right: 5px;
+	font-family: Apple SD Gothic Neo;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 12px;
+	line-height: 14px;
+	color: #ffffff;
+	text-align: center;
+	line-height: 19px;
+`;
+
+export default RoomCategoryStyled;
