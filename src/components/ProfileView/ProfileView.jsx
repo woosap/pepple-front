6267042f731f5212c07c2b0057f9386277d5927e
from97 <skm1104@daum.net>
2021-07-06 19:30:55 +0900

    ProfileView 컴포넌트 작성 및 스타일링

--- src/components/ProfileView/ProfileView.jsx
+++ src/components/ProfileView/ProfileView.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import ProfileViewStyled from './ProfileView.styles';
+import {
+	ProfileViewStyled,
+	ProfileImage,
+	UserInfo,
+	ModifyProfileButton,
+	SNSList,
+} from './ProfileView.styles';
 
-const ProfileView = () => {
-	return <ProfileViewStyled>Profile</ProfileViewStyled>;
+const ProfileView = ({ user }) => {
+	return (
+		<ProfileViewStyled>
+			<ProfileImage />
+			<UserInfo>
+				<UserInfo.Name>{user.name}</UserInfo.Name>
+				<UserInfo.Job>{user.job}</UserInfo.Job>
+				<UserInfo.Description>{user.description}</UserInfo.Description>
+			</UserInfo>
+			<ModifyProfileButton>개인정보 수정</ModifyProfileButton>
+			<SNSList>
+				{user.sns.map(item => (
+					<SNSList.Item key={item.id} sort={item.sort} href={item.link} />
+				))}
+			</SNSList>
+		</ProfileViewStyled>
+	);
 };
 
 export default ProfileView;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import ProfileView from '../components/ProfileView/ProfileView';
 
-const MainPage = () => {
+const MainPage = ({ user }) => {
 	return (
 		<>
 			<Header />
-			<ProfileView />
+			<ProfileView user={user} />
 		</>
 	);
 };
 
 export default MainPage;
+
+MainPage.defaultProps = {
+	user: {
+		id: 1,
+		name: '쭈꾸미 개발자',
+		job: 'FRONTEND',
+		description:
+			'쭈꾸미처럼 맛있게 성장하고 싶은 쭈꾸미 프론트엔드 개발자 입니다. 쭈꾸미처럼 맛있게 성장하고 싶은 쭈꾸미 프론트엔드 개발자 입니다',
+		sns: [
+			{ id: 1, sort: 'blog', link: 'blog_link' },
+			{ id: 2, sort: 'github', link: 'github_link' },
+			{ id: 3, sort: 'instagram', link: 'instagram_link' },
+			{ id: 4, sort: 'facebook', link: 'facebook_link' },
+		],
+	},
+};
