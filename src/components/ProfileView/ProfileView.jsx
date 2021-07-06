bc02bd8d51228e250d72b7a47e42e8d4f926907c
from97 <skm1104@daum.net>
2021-07-06 18:45:42 +0900

    ProfileView 생성 및 연결

---
+++ src/components/ProfileView/ProfileView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProfileViewStyled from './ProfileView.styles';
+
+const ProfileView = () => {
+	return <ProfileViewStyled>Profile</ProfileViewStyled>;
+};
+
+export default ProfileView;
---
+++ src/components/ProfileView/ProfileView.styles.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const ProfileViewStyled = styled.div``;
+
+export default ProfileViewStyled;
