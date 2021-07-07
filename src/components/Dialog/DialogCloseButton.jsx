f74264b559ff60d69ea92cbbbd5f7234ef512ce4
from97 <skm1104@daum.net>
2021-07-08 02:03:50 +0900

    DialogCloseButton 스타일링

--- src/components/Dialog/DialogCloseButton.jsx
+++ src/components/Dialog/DialogCloseButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CloseButton } from './DialogCloseButton.styles';
+import CloseButton from './DialogCloseButton.styles';
 import { ReactComponent as CloseIcon } from '../../assets/icon/icon-close.svg';
 
 const DialogCloseButton = ({ onCloseButton }) => (
