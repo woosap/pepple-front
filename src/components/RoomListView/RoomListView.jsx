d3d09a04fd53d525e7f0339005dc163a9e503073
from97 <skm1104@daum.net>
2021-07-08 01:40:10 +0900

    방 새로 만들기 버튼 생성 시 Dialog 띄우기

--- src/components/RoomListView/RoomListView.jsx
+++ src/components/RoomListView/RoomListView.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import { OverlayContainer } from '@react-aria/overlays';
 import {
 	RoomListViewStyled,
 	RoomList,
 	CreateNewRoomButton,
 } from './RoomListView.styles';
+import useToggleDialog from '../../hooks/useToggleDialog';
+import Dialog from '../Dialog/Dialog';
+
 import RoomListItem from '../RoomListItem/RoomListItem';
 
 const RoomListView = ({ rooms }) => {
+	const { state, openButtonProps, openButtonRef } = useToggleDialog();
+
 	return (
-		<RoomListViewStyled>
-			<RoomList>
-				{rooms.map(room => (
-					<RoomListItem key={room.id} room={room} />
-				))}
-			</RoomList>
-			<CreateNewRoomButton>방 새로 만들기</CreateNewRoomButton>
-		</RoomListViewStyled>
+		<>
+			<RoomListViewStyled>
+				<RoomList>
+					{rooms.map(room => (
+						<RoomListItem key={room.id} room={room} />
+					))}
+				</RoomList>
+				<CreateNewRoomButton {...openButtonProps} ref={openButtonRef}>
+					방 새로 만들기
+				</CreateNewRoomButton>
+			</RoomListViewStyled>
+			{state.isOpen && (
+				<OverlayContainer>
+					<Dialog />
+				</OverlayContainer>
+			)}
+		</>
 	);
 };
 
