622ee4c30c5a69040b6b063ec7c275c673b829fb
from97 <skm1104@daum.net>
2021-07-08 01:47:19 +0900

    DialogCloseButton 컴포넌트 생성

---
+++ src/components/Dialog/DialogCloseButton.styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const CloseButton = styled.button`
+	position: absolute;
+	top: 16px;
+	right: 25px;
+	cursor: pointer;
+
+	svg {
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+export default CloseButton;
