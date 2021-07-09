3762f6e59a8a8ac53b13d9ed858448af121ced4b
from97 <skm1104@daum.net>
2021-07-09 23:47:26 +0900

    RoomProfileView 컴포넌트 작성

--- src/components/RoomProfileView/RoomProfileView.styles.jsx
+++ src/components/RoomProfileView/RoomProfileView.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const RoomProfileViewStyled = styled.div`
+export const RoomProfileViewStyled = styled.div`
 	width: 100%;
 	height: 120px;
 	background-color: #fff;
@@ -8,6 +8,28 @@ const RoomProfileViewStyled = styled.div`
 	box-sizing: border-box;
 	border-radius: 30px;
 	margin: 40px 0 27px 0;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 4%;
 `;
 
-export default RoomProfileViewStyled;
+export const RoomTitle = styled.h1`
+	font-size: 20px;
+	font-weight: 500;
+	color: #3e217e;
+`;
+
+export const RoomInfo = styled.div`
+	position: relative;
+`;
+
+RoomInfo.CategoryList = styled.div`
+	display: flex;
+`;
+
+RoomInfo.BirthTime = styled.div`
+	font-size: 12px;
+	margin: 12px 6px 0 0;
+	text-align: end;
+`;
