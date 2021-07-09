ecd5a0408d3f71e5b312c95a97d1861cd4c7f82c
from97 <skm1104@daum.net>
2021-07-09 22:42:12 +0900

    RoomCloudView 레이아웃 잡기

--- src/components/RoomCloudView/RoomCloudView.styles.jsx
+++ src/components/RoomCloudView/RoomCloudView.styles.jsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
-const RoomCloudViewStyled = styled.div``;
+const RoomCloudViewStyled = styled.div`
+	width: 100%;
+	height: 700px;
+	background-color: #fff;
+	border: 1px solid #dadcf3;
+	box-sizing: border-box;
+	border-radius: 20px;
+	margin: 40px 0 0 30px;
+`;
 
 export default RoomCloudViewStyled;
