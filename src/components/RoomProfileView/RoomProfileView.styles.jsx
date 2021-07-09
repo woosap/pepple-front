55779ddeb8ee5ac78b9ca92d787077daa66f09e2
from97 <skm1104@daum.net>
2021-07-09 22:42:21 +0900

    RoomProfileView 레이아웃 잡기

--- src/components/RoomProfileView/RoomProfileView.styles.jsx
+++ src/components/RoomProfileView/RoomProfileView.styles.jsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
-const RoomProfileViewStyled = styled.div``;
+const RoomProfileViewStyled = styled.div`
+	width: 100%;
+	height: 120px;
+	background-color: #fff;
+	border: 1px solid #dadcf3;
+	box-sizing: border-box;
+	border-radius: 30px;
+	margin: 40px 0 27px 0;
+`;
 
 export default RoomProfileViewStyled;
