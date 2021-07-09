93f472c95b3c08dbe7d29ffbf77aed7a2f0da8b9
from97 <skm1104@daum.net>
2021-07-09 22:41:29 +0900

    RoomMemberListView 컴포넌트 작성 배치

--- src/components/RoomMemberListView/RoomMemberListView.jsx
+++ src/components/RoomMemberListView/RoomMemberListView.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
-import RoomMemberListViewStyled from './RoomMemberListView.styles';
+import {
+	RoomMemberListViewStyled,
+	MemberWrapper,
+	MemberName,
+	MemberJob,
+	ProfileImageWrapper,
+} from './RoomMemberListView.styles';
+import ProfileImage from '../ProfileImage/ProfileImage';
+import { ReactComponent as MikeIcon } from '../../assets/icon/icon-mike.svg';
 
-const RoomMemberListView = () => <RoomMemberListViewStyled />;
+const RoomMemberListView = ({ members }) => (
+	<RoomMemberListViewStyled>
+		{members.map(member => (
+			<MemberWrapper key={member.id}>
+				<MemberWrapper.Left>
+					<ProfileImageWrapper>
+						<ProfileImage size="medium" />
+					</ProfileImageWrapper>
+				</MemberWrapper.Left>
+				<MemberWrapper.Right>
+					<MemberName>{member.name}</MemberName>
+					<MemberJob>{member.job}</MemberJob>
+					<MikeIcon />
+				</MemberWrapper.Right>
+			</MemberWrapper>
+		))}
+	</RoomMemberListViewStyled>
+);
 
 export default RoomMemberListView;
--- src/components/RoomMemberListView/RoomMemberListView.styles.jsx
+++ src/components/RoomMemberListView/RoomMemberListView.styles.jsx
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
 
-const RoomMemberListViewStyled = styled.div``;
+export const RoomMemberListViewStyled = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 24px;
+	height: 500px;
+`;
 
-export default RoomMemberListViewStyled;
+export const MemberWrapper = styled.div`
+	display: flex;
+	background-color: #fff;
+	border: 1px solid #d0d4ff;
+	box-sizing: border-box;
+	border-radius: 35px;
+`;
+
+MemberWrapper.Left = styled.div`
+	display: flex;
+	margin: 30px;
+	align-items: center;
+`;
+
+MemberWrapper.Right = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+export const MemberName = styled.div`
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 23px;
+	color: #3e217e;
+`;
+
+export const MemberJob = styled.div`
+	font-size: 10px;
+	line-height: 14px;
+	color: #b7b7b7;
+	margin-bottom: 14px;
+`;
+
+export const ProfileImageWrapper = styled.div``;
