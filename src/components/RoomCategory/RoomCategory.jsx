a10d4c8e7923bd024007dbf49a15e4f3854c1030
from97 <skm1104@daum.net>
2021-07-11 20:14:24 +0900

    RoomCategory 컴포넌트 mapping key props 추가

--- src/components/RoomCategory/RoomCategory.jsx
+++ src/components/RoomCategory/RoomCategory.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import RoomCategoryStyled from './RoomCategory.styles';
 
 const RoomCategory = ({ category, categories }) => {
-	return (
-		<RoomCategoryStyled key={categories[category].id} category={category}>
-			{categories[category].ko}
-		</RoomCategoryStyled>
-	);
+	return <RoomCategoryStyled>{categories[category].ko}</RoomCategoryStyled>;
 };
 
 export default RoomCategory;
