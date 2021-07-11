5960822fded6c73017df6ea3fd727910962d9816
from97 <skm1104@daum.net>
2021-07-11 21:29:45 +0900

    MuteButton hovering, clicked 효과 적용

--- src/components/MuteButton/MuteButton.jsx
+++ src/components/MuteButton/MuteButton.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MuteButtonStyled from './MuteButton.styles';
 
-const MuteButton = () => <MuteButtonStyled>음성 끄기</MuteButtonStyled>;
+const MuteButton = () => {
+	const [isMute, setIsMute] = useState(true);
+	const handleMuteClick = () => {
+		setIsMute(prev => !prev);
+	};
+	return (
+		<MuteButtonStyled isMute={isMute} onClick={handleMuteClick}>
+			{isMute ? '음성 켜기' : '음성 끄기'}
+		</MuteButtonStyled>
+	);
+};
 
 export default MuteButton;
