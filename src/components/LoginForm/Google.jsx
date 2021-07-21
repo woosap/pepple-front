a8c7bbef438a3ecaef72ea04759580507c5a4d68
from97 <skm1104@daum.net>
2021-07-21 19:11:22 +0900

    구글 로그인 수정

--- src/components/LoginForm/Google.jsx
+++ src/components/LoginForm/Google.jsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import GoogleLogin from 'react-google-login';
 import styled from 'styled-components';
 
 const Google = () => {
-	const [userId, setUserId] = useState();
-	const [userEmail, setUserEmail] = useState('');
-	const [provider, setProvider] = useState('google');
-	const [accessToken, setAccessToken] = useState('');
-
 	const onSuccess = res => {
-		setUserId(res.profileObj.googleId);
-		setUserEmail(res.profileObj.email);
-		setProvider('google');
-		setAccessToken(res.accessToken);
-		console.log(userId, userEmail, provider, accessToken);
+		console.log(res);
 	};
 
 	const onFailure = error => {
