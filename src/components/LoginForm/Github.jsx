6be5265d71b36caa5176c43d27ccc3eca3ee99bb
from97 <skm1104@daum.net>
2021-07-29 20:41:13 +0900

    Github 컴포넌트 구현

---
+++ src/components/LoginForm/Github.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GithubLogin = () => {
+	const uri = `https://github.com/login/oauth/authorize?client_id=c626d47b6711b219e86c&redirect_uri=http://localhost:3000/redirect`;
+	return <GithubLoginStyled href={uri}>Github</GithubLoginStyled>;
+};
+
+const GithubLoginStyled = styled.a`
+	background: none;
+	cursor: pointer;
+	font-size: 12px;
+	font-family: Apple SD Gothic Neo;
+	letter-spacing: 0.1em;
+	color: #fff;
+`;
+
+export default GithubLogin;
