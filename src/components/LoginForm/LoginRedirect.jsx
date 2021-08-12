60b8c29887837f69a5da6f07f450231e74cb00c7
from97 <skm1104@daum.net>
2021-08-12 17:30:58 +0900

    LoginRedirect 컴포넌트 수정

--- src/components/LoginForm/LoginRedirect.jsx
+++ src/components/LoginForm/LoginRedirect.jsx
@@ -1,37 +1,15 @@
 import React, { useEffect } from 'react';
 import qs from 'qs';
-import axios from 'axios';
 
 const LoginRedirect = ({ location, history }) => {
 	useEffect(() => {
-		async function getToken() {
-			const { code } = qs.parse(location.search, {
+		function getToken() {
+			const { token } = qs.parse(location.search, {
 				ignoreQueryPrefix: true,
 			});
 
-			const clientID = 'c626d47b6711b219e86c';
-			const clientSecret = '';
-
-			const response = await axios.post(
-				'https://github.com/login/oauth/access_token',
-				{
-					code,
-					clientID,
-					clientSecret,
-				},
-				{
-					headers: {
-						accept: 'application/json',
-					},
-				},
-			);
-
-			const token = response.data.access_token;
-
-			console.log(`github token: ${token}`);
-			console.log(location);
+			console.log(token);
 		}
-
 		getToken();
 		history.push('/');
 	}, [location]);
