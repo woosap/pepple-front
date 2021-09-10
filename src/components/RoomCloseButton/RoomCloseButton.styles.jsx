e138957517bfb540c6ee7d2a9a0ef7890609171b
from97 <skm1104@daum.net>
2021-09-10 19:57:33 +0900

    방 종료 모달 구현

--- src/components/RoomCloseButton/RoomCloseButton.styles.jsx
+++ src/components/RoomCloseButton/RoomCloseButton.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const RoomCloseButtonStyled = styled.button`
+export const RoomCloseButtonStyled = styled.button`
 	width: 145px;
 	height: 48px;
 	background-color: #fff;
@@ -17,4 +17,72 @@ const RoomCloseButtonStyled = styled.button`
 	}
 `;
 
-export default RoomCloseButtonStyled;
+export const RoomCloseModal = styled.div`
+	width: 423px;
+	height: 185px;
+	position: absolute;
+	right: 0px;
+	bottom: 55px;
+	background-color: #fff;
+	border: 1px solid #d0d4ff;
+	box-sizing: border-box;
+	box-shadow: 0px 4px 40px rgba(147, 147, 147, 0.1);
+	border-radius: 20px;
+	z-index: 100;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 35px;
+
+	p {
+		width: 88%;
+		text-align: left;
+	}
+
+	svg {
+		position: absolute;
+		top: 18px;
+		right: 22px;
+		cursor: pointer;
+	}
+`;
+
+RoomCloseModal.Title = styled.p`
+	font-weight: bold;
+	font-size: 16px;
+	line-height: 23px;
+	letter-spacing: -0.03em;
+	color: #6138b9;
+	margin-bottom: 5px;
+	display: flex;
+	align-items: center;
+	.icon {
+		margin-top: -2px;
+		margin-left: 8px;
+		font-size: 21px;
+		display: inline-block;
+		height: 21px;
+	}
+`;
+
+RoomCloseModal.Description = styled.p`
+	font-weight: 300;
+	font-size: 14px;
+	line-height: 20px;
+	letter-spacing: -0.03em;
+	color: #686868;
+	margin-bottom: 40px;
+`;
+
+RoomCloseModal.Button = styled.button`
+	width: 110px;
+	height: 36px;
+	background: #6138b9;
+	border-radius: 40px;
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 20px;
+	letter-spacing: -0.03em;
+	color: #ffffff;
+	cursor: pointer;
+`;
