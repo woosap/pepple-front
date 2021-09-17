44f2d7e3cbe10286576ca7ae68f36257a8ba1502
from97 <skm1104@daum.net>
2021-09-18 00:24:14 +0900

    ProfileImage 렌더링

--- src/components/ProfileImage/ProfileImage.jsx
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ProfileImageStyled from './ProfileImage.styles';
+import defaultImg from '../../assets/img-default.svg';
 
-const ProfileImage = ({ size, order }) => (
-	<ProfileImageStyled size={size} order={order} />
+const ProfileImage = ({ url, size, order }) => (
+	<ProfileImageStyled size={size} order={order}>
+		{!url && <img src={defaultImg} alt="profile" />}
+		{url && <img src={url} alt="profile" />}
+	</ProfileImageStyled>
 );
 
 export default ProfileImage;
