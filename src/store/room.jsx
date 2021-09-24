7e5b7e78f2bce13094b13d3c2562c5ffd216a7ab
from97 <skm1104@daum.net>
2021-09-24 16:10:40 +0900

    room context 작성

--- src/store/room.jsx
+++ src/store/room.jsx
@@ -1,15 +1,107 @@
-import React, { createContext } from 'react';
+import React, {
+	createContext,
+	useContext,
+	useState,
+	useLayoutEffect,
+} from 'react';
+import AuthContext from './auth';
+import api from '../api';
 
-const RoomContext = createContext({});
+const RoomContext = createContext({
+	rooms: [],
+	createRoom: () => {},
+	enterRoom: () => {},
+	getTime: () => {},
+});
 
 const RoomProvider = ({ children }) => {
-	const value = {};
+	const { userId } = useContext(AuthContext).state;
+	const [rooms, setRooms] = useState([]);
+	console.log(rooms);
+	useLayoutEffect(() => {
+		api
+			.get(`/room?pageNumber=${0}&pageSize=${10}`)
+			.then(res => {
+				setRooms([...res.data]);
+			})
+			.catch(err => console.log(err));
+	}, []);
 
-	return (
-		<RoomContext.RoomProvider value={value}>
-			{children}
-		</RoomContext.RoomProvider>
-	);
+	const createRoom = (title, subTitle, capacity, category) => {
+		const token = localStorage.getItem('token');
+		api
+			.post(
+				`/room/create`,
+				{
+					capacity,
+					category,
+					subTitle,
+					title,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				},
+			)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	};
+
+	const enterRoom = roomId => {
+		const token = localStorage.getItem('token');
+		api
+			.post(
+				`/room/enter`,
+				{
+					roomId,
+					userId,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				},
+			)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	};
+
+	const getRoomDetail = roomId => {
+		api
+			.get(`/room/${roomId}`)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	};
+
+	const getTime = room => {
+		const now = new Date();
+		const birth = new Date(room.date);
+
+		const timeMinutes = Math.floor(
+			(now.getTime() - birth.getTime()) / 1000 / 60,
+		);
+		if (timeMinutes < 1) return '방금전';
+		if (timeMinutes < 60) return `${timeMinutes}분전`;
+
+		const timeHours = Math.floor(timeMinutes / 60);
+		if (timeHours < 24) return `${timeHours}시간 전`;
+
+		const timeDays = Math.floor(timeMinutes / 60 / 24);
+		if (timeDays < 365) return `${timeDays}일전`;
+
+		return `${Math.floor(timeDays / 365)}년 전`;
+	};
+
+	const value = {
+		rooms,
+		createRoom,
+		enterRoom,
+		getTime,
+		getRoomDetail,
+	};
+
+	return <RoomContext.Provider value={value}>{children}</RoomContext.Provider>;
 };
 
 const { Consumer: RoomConsumer } = RoomContext;
