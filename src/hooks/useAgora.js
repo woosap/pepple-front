0b76df2d42eec4606f62aa13c23e37af8750fba6
from97 <skm1104@daum.net>
2021-10-02 16:11:46 +0900

    agora 커스텀 훅 - join channel, get agora token 수정

--- src/hooks/useAgora.js
+++ src/hooks/useAgora.js
@@ -1,21 +1,41 @@
 import AgoraRTC from 'agora-rtc-sdk-ng';
+import api from '../api';
 
-const useAgora = (userId, roomId) => {
+const useAgora = () => {
 	const rtc = {
-		client: null,
+		client: AgoraRTC.createClient({ mode: 'rtc', codec: 'h264' }),
 		localAudioTrack: null,
 	};
 
 	const options = {
 		appId: process.env.REACT_APP_AGORA_APP_ID,
-		channel: roomId,
-		token: null,
-		uid: userId,
+		channel: '',
+		uid: '',
 	};
 
 	// get agora token
-	const getAgoraToken = () => {
-		options.token = null;
+	const getAgoraToken = async (userId, roomId) => {
+		const token = localStorage.getItem('token');
+		options.channel = String(roomId);
+		options.uid = userId;
+		try {
+			const res = await api.post(
+				`/agoraToken`,
+				{
+					channelName: options.channel,
+					userAccount: options.uid,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				},
+			);
+			return res.data.token;
+		} catch (err) {
+			console.log(err);
+			return null;
+		}
 	};
 
 	// remote user published
@@ -32,18 +52,22 @@ const useAgora = (userId, roomId) => {
 	};
 
 	// enter room
-	const joinChannel = async () => {
+	const joinChannel = async (userId, roomId) => {
 		try {
-			rtc.client = AgoraRTC.createClient({ mode: 'rtc', codec: 'h264' });
+			const agoraToken = await getAgoraToken(userId, roomId);
 			rtc.client.on('user-published', handleUserPublished);
 			rtc.client.on('user-unpublished', handleUserUnpublished);
-			rtc.client.join(
-				options.appId,
-				options.channel,
-				options.token,
-				options.uid,
-			);
-			rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+			if (agoraToken) {
+				await rtc.client
+					.join(options.appId, options.channel, agoraToken, options.uid)
+					.then(res => console.log('join success !', res))
+					.catch(err => console.log(err));
+				rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+				await rtc.client.publish([rtc.localAudioTrack]);
+				console.log('publish success!');
+			} else {
+				console.log('agora token missing');
+			}
 		} catch (err) {
 			console.log(err);
 		}
@@ -51,8 +75,13 @@ const useAgora = (userId, roomId) => {
 
 	// leave room
 	const leaveChannel = async () => {
-		rtc.localAudioTrack.close();
-		await rtc.client.leave();
+		console.log(rtc);
+		if (rtc.localAudioTrack) {
+			await rtc.localAudioTrack.close();
+		}
+		if (rtc.client) {
+			await rtc.client.leave();
+		}
 	};
 
 	const mute = () => {
@@ -63,7 +92,7 @@ const useAgora = (userId, roomId) => {
 		rtc.localAudioTrack.setEnabled(true);
 	};
 
-	return { getAgoraToken, joinChannel, leaveChannel, mute, unmute };
+	return { joinChannel, leaveChannel, mute, unmute };
 };
 
 export default useAgora;
