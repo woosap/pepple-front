190ab104ead35a9381f11aaef56204e9e5d52dda
from97 <skm1104@daum.net>
2021-10-03 19:39:53 +0900

    room detail page 새로고침 시 agora 끊어지는 이슈 해결

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -7,18 +7,35 @@ import RoomCloudView from '../components/RoomCloudView/RoomCloudView';
 import MuteButton from '../components/MuteButton/MuteButton';
 import RoomCloseButton from '../components/RoomCloseButton/RoomCloseButton';
 import RoomContext from '../store/room';
+import useAgora from '../hooks/useAgora';
 
 const DetailPage = ({ match }) => {
-	const { roomInfo, users, getTime, getRoomDetail, leaveRoom } =
-		useContext(RoomContext);
+	const {
+		roomInfo,
+		users,
+		getTime,
+		getRoomDetail,
+		leaveRoom,
+		setLocalAudioTrack,
+	} = useContext(RoomContext);
 	const [time, setTime] = useState('');
+	const { joinChannel } = useAgora();
 
 	useLayoutEffect(() => {
-		getRoomDetail(Number(match.params.roomId));
+		const roomId = Number(match.params.roomId);
+		const userId = localStorage.getItem('user');
+		getRoomDetail(roomId);
+		const agora = async () => {
+			const track = await joinChannel(userId, roomId);
+			setLocalAudioTrack(track);
+		};
+		agora();
 	}, []);
 
 	useEffect(() => {
-		if (roomInfo) setTime(getTime(roomInfo));
+		if (roomInfo) {
+			setTime(getTime(roomInfo));
+		}
 	}, [roomInfo]);
 
 	const handleLeaveRoom = () => {
