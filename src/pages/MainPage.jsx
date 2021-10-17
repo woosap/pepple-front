6c9a3b69a07f34860015b393de49a81afc9e850e
from97 <skm1104@daum.net>
2021-10-17 15:57:56 +0900

    Main Page SWR 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useLayoutEffect } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { OverlayContainer } from '@react-aria/overlays';
 import Header from '../components/Header/Header';
@@ -8,16 +8,9 @@ import Dialog from '../components/Dialog/Dialog';
 import LoginForm from '../components/LoginForm/LoginForm';
 import ProfileForm from '../components/ProfileForm/ProfileForm';
 import AuthContext from '../store/auth';
-import RoomContext from '../store/room';
 
 const MainPage = () => {
-	const authContext = useContext(AuthContext);
-	const { joined, logined } = authContext.state;
-	const { getRooms } = useContext(RoomContext);
-
-	useLayoutEffect(() => {
-		getRooms();
-	}, []);
+	const { joined, logined } = useContext(AuthContext).state;
 
 	return (
 		<>
