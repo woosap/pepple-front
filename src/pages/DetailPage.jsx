cf30331f04f66cd994bf8ee62fbe7909f366aeb8
from97 <skm1104@daum.net>
2021-10-21 02:00:46 +0900

    종료 버튼 클릭 이슈 해결

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,20 +1,24 @@
-import React, { useContext, useLayoutEffect } from 'react';
+import React, { useContext, useLayoutEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header/Header';
 import RoomProfileView from '../components/RoomProfileView/RoomProfileView';
 import RoomMemberListView from '../components/RoomMemberListView/RoomMemberListView';
 import RoomCloudView from '../components/RoomCloudView/RoomCloudView';
 import MuteButton from '../components/MuteButton/MuteButton';
-import RoomCloseButton from '../components/RoomCloseButton/RoomCloseButton';
 import CustomPrompt from '../components/Prompt/CustomPrompt';
 import RoomContext from '../store/room';
 import { useRoomData } from '../hooks/useRoomData';
 import useAgora from '../hooks/useAgora';
+import RoomCloseModal from '../components/RoomCloseModal/RoomCloseModal';
+import { RoomCloseButtonStyled } from '../components/RoomCloseButton/RoomCloseButton.styles';
 
 const DetailPage = ({ match }) => {
 	const { getRoomData } = useRoomData(match.params.roomId);
 	const { getTime, leaveRoom, setLocalAudioTrack } = useContext(RoomContext);
 	const { joinChannel } = useAgora();
+	const [clicked, setClicked] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [shouldConfirm, setShoudConfirm] = useState(true);
 
 	useLayoutEffect(() => {
 		const roomId = Number(match.params.roomId);
@@ -28,6 +32,12 @@ const DetailPage = ({ match }) => {
 
 	const handleLeaveRoom = () => {
 		leaveRoom(match.params.roomId);
+	};
+
+	const handleClick = () => {
+		setClicked(prev => !prev);
+		setShoudConfirm(prev => !prev);
+		setIsOpen(prev => !prev);
 	};
 
 	if (getRoomData.error) {
@@ -64,12 +74,20 @@ const DetailPage = ({ match }) => {
 				</DetailContainer.Left>
 				<DetailContainer.Right>
 					<RoomCloudView />
-					<RoomCloseButton handleLeaveRoom={handleLeaveRoom} />
+					<RoomCloseButtonStyled clicked={clicked} onClick={handleClick}>
+						종료
+					</RoomCloseButtonStyled>
+					{isOpen && (
+						<RoomCloseModal
+							handleLeave={handleLeaveRoom}
+							handleCancle={handleClick}
+						/>
+					)}
 				</DetailContainer.Right>
 			</DetailContainer>
 			<CustomPrompt
 				match={match}
-				shouldConfirm
+				shouldConfirm={shouldConfirm}
 				handleLeaveRoom={handleLeaveRoom}
 			/>
 		</>
