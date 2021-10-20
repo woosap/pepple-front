dfd95c35fa541431a4602a47f1f33d7534543884
from97 <skm1104@daum.net>
2021-10-21 02:00:37 +0900

    RoomCloseModal 적용

--- src/components/Prompt/CustomPrompt.jsx
+++ src/components/Prompt/CustomPrompt.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { Prompt } from 'react-router-dom';
-import { OverlayContainer } from '@react-aria/overlays';
-import CustomModalStyled from './CustomPrompt.styles';
-import Dialog from '../Dialog/Dialog';
-import { ReactComponent as CloseIcon } from '../../assets/icon/icon-close-mini.svg';
+import RoomCloseModal from '../RoomCloseModal/RoomCloseModal';
 
 const CustomPrompt = ({ match, shouldConfirm, handleLeaveRoom }) => {
 	const [modalOpen, setModalOpen] = useState(false);
@@ -35,27 +32,7 @@ const CustomPrompt = ({ match, shouldConfirm, handleLeaveRoom }) => {
 		<>
 			<Prompt when={shouldConfirm} message={handleNavigation} />
 			{modalOpen && (
-				<OverlayContainer>
-					<Dialog type="alert">
-						<CustomModalStyled>
-							<CustomModalStyled.Title>
-								채팅을 종료하시겠습니까 ?<span className="icon">🤫</span>
-							</CustomModalStyled.Title>
-							<CustomModalStyled.Description>
-								채팅이 종료되면 클라우드 파일은 저장되지 않습니다.
-							</CustomModalStyled.Description>
-							<CustomModalStyled.ButtonWrapper>
-								<CustomModalStyled.Button type="leave" onClick={handleLeave}>
-									나가기
-								</CustomModalStyled.Button>
-								<CustomModalStyled.Button type="cancle" onClick={handleCancle}>
-									취소
-								</CustomModalStyled.Button>
-							</CustomModalStyled.ButtonWrapper>
-							<CloseIcon onClick={handleCancle} />
-						</CustomModalStyled>
-					</Dialog>
-				</OverlayContainer>
+				<RoomCloseModal handleLeave={handleLeave} handleCancle={handleCancle} />
 			)}
 		</>
 	);
