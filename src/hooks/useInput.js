a8d6dce17c5bdb351d3ca6c489fe9e88c2cd25f6
from97 <skm1104@daum.net>
2021-10-22 14:50:30 +0900

    CreateRoomForm user error 핸들링: useInput에 ref 추가

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const useInput = (initialValue, checker) => {
 	const [value, setValue] = useState(initialValue);
 	const [isFilled, setIsFilled] = useState(true);
+	const ref = useRef();
 	const onChange = e => {
 		if (typeof checker === 'function') {
 			if (checker(e.target.value)) {
@@ -19,7 +20,12 @@ const useInput = (initialValue, checker) => {
 			setIsFilled(false);
 		}
 	};
-	return { value, isFilled, onChange, onBlur, setIsFilled };
+
+	useEffect(() => {
+		if (!isFilled) ref.current.focus();
+	}, [isFilled]);
+
+	return { value, isFilled, onChange, onBlur, setIsFilled, ref };
 };
 
 export default useInput;
